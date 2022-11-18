6c4c8370e25ab608c2055c36c7f4d1462baf73fa
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-11-18 20:48:30 -0300

    update

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ export default function Home() {
         <title>InnerCase</title>
         <meta name="description" content="Letter and word conversion tool... The text tool takes all uppercase letters entered in the text area and converts them to lowercase letters, counts the number of characters entered in the text area, among other conversion features." />
         <link rel="icon" href="/logo.png" />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2417167677821960"
+          crossorigin="anonymous"></script>
       </Head>
       <Initial />
 

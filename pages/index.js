f080fd17742a1d8ddd89c02e1414b7ee0cef7194
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-12-01 10:56:01 -0300

    lang added

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import Initial from './initial'
 
+
 export default function Home() {
   return (
     <>
-      <Head lang="pt-br">
-        
+      <Head>
         <title>Convert Small Case</title>
         <meta name="description" content="Letter and word conversion tool... The text tool takes all uppercase letters entered in the text area and converts them to lowercase letters, counts the number of characters entered in the text area, among other conversion features." />
         <meta charset="utf-8" />

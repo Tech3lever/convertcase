5077bf47c69cbd8a0359acc29fe5cac4f05497e1
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-12-01 10:43:57 -0300

    ok

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,17 @@ export default function Home() {
       <Head>
         <title>Convert Small Case</title>
         <meta name="description" content="Letter and word conversion tool... The text tool takes all uppercase letters entered in the text area and converts them to lowercase letters, counts the number of characters entered in the text area, among other conversion features." />
-
+        <meta charset="utf-8" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Convert Small Case" />
+        <meta property="og:description" content="Letter and word conversion tool... The text tool takes all uppercase letters entered in the text area and converts them to lowercase letters, counts the number of characters entered in the text area, among other conversion features." />
+        <meta property="og:locale" content="pt_BR" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta name="robots" content="index,follow" />
+        <meta name="googlebot" content="index,follow" />
+        <meta property="og:url" content="https://convertsmallcase.online/" />
+        <link rel="canonical" href="https://convertsmallcase.online/" />
+        
         {/* icons */}
         <link rel="icon" href="/logo.png" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />

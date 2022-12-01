cc9ab4b71fbceefeb773092e87598ddd7ac94369
Rafael-doctom <dasilva.rafaelpt@gmail.com>
2022-12-01 13:38:23 -0300

    ok

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ["pt"],
+    defaultLocale: "pt",
+  },
+}
+
+
+
+module.exports = nextConfig
